b5c9b282c8978f5599476e133767111f8aec5783
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2023-08-30 08:32:48 -0400

    [ART-15] Add basic store file

---
+++ app/src/store.js
@@ -0,0 +1,27 @@
+const getStoreData = (key) => {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    console.error(err)
+    return null;
+  }
+};
+
+const saveArtworks = (artworks) => {
+  localStorage.setItem('artworks', JSON.stringify(artworks));
+};
+
+// const artwork = {
+//   id: 23119,
+//   image_id: '1e452e34-3a2b-0dca-35c3-c7236c612985',
+//   title: 'Village Among the Trees',
+// };
+
+export const getSavedArtworks = () => {
+  const artworks = getStoreData('artworks')
+  return (artworks) ? artworks : [];
+};
+
+export const addSavedArtworks = (artwork) => {
+  saveArtworks([...getSavedArtworks(), artwork]);
+}
